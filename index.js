efc5f987081fcf86a57efd475dc54f15875d1ffb
MD SAHID AHMED <iamsahidofficial99@gmail.com>
2024-04-28 15:05:19 +0600

    added updated data from client side

--- index.js
+++ index.js
@@ -45,22 +45,55 @@ async function run() {
       res.send(result);
     });
 
-    // get data for specific id's
-
-    app.get("/mycrafts/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await artsCollection.findOne(query);
-      res.send(result);
-    });
-
     // get data by email from mongodb
 
     app.get("/mycrafts/:email", async (req, res) => {
-      const query = req.params.email;
-      const result = await artsCollection.find({ email: query }).toArray();
+      const result = await artsCollection
+        .find({ email: req.params.email })
+        .toArray();
       res.send(result);
     });
+
+    // get data with specific id
+
+    app.get("/updatecraft/:id", async (req, res) => {
+      const result = await artsCollection.findOne({
+        _id: new ObjectId(req.params.id),
+      });
+
+      res.send(result);
+    });
+
+    // update data from client side
+
+    app.put("/updatecraft/:id", async (req, res) => {
+      console.log(req.params.id);
+      const query = { _id: new ObjectId(req.params.id) };
+      const data = {
+        $set: {
+          image: req.body.image,
+          item_name: req.body.item_name,
+          subcategory_name: req.body.subcategory_name,
+          short_description: req.body.short_description,
+          rating: req.body.rating,
+          price: req.body.price,
+          scustom: req.body.scustom,
+          process_time: req.body.process_time,
+          stock: req.body.stock,
+        },
+      };
+      const result = await artsCollection.updateOne(query, data);
+      res.send(result);
+    });
+
+    // get data for specific id's
+
+    // app.get("/mycrafts/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: new ObjectId(id) };
+    //   const result = await artsCollection.findOne(query);
+    //   res.send(result);
+    // });
 
     //delete data from client site
 
